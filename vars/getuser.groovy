f2668d1c7101cdf522792b7e6a71ef77c800ca99
Saul Ortigoza <saul.ortigoza@wizeline.com>
2017-12-12 11:04:52 -0600

    get user function 2

--- vars/getuser.groovy
+++ vars/getuser.groovy
@@ -1,5 +1,11 @@
 def call(){
-    wrap([$class: 'BuildUser']) {
-      return BUILD_USER
-    }
+  
+  def build = currentBuild.rawBuild
+  def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+  def name = cause.getUserName()
+  return name
+
+    //wrap([$class: 'BuildUser']) {
+    //  return BUILD_USER
+    //}
 }
