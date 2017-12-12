a874ef67791a690613ad5384825fec9bb83b18ae
Saul Ortigoza <saul.ortigoza@wizeline.com>
2017-12-12 12:23:28 -0600

    closure local vars

--- vars/genericDispatcher.groovy
+++ vars/genericDispatcher.groovy
@@ -15,29 +15,12 @@ def call(body) {
   def gitCredentialsId = params.GIT_CREDENTIALS_ID
   def gitSha = params.BRANCH
 
-  // deleteDir()
-  //
-  // stage ('Checkout') {
-  //   git branch: gitSha, url: gitRepoUrl, credentialsId: gitCredentialsId
-  //   // git branch: '**', url: "${params.GIT_REPO_URL}", credentialsId: "${params.GIT_CREDENTIALS_ID}"
-  //   gitBranch = sh(returnStdout:true, script:'git rev-parse --abbrev-ref HEAD').trim()
-  //   gitSha = sh(returnStdout:true, script:'git rev-parse HEAD').trim()
-  //
-  //   echo "Branch: ${gitBranch}"
-  //   echo "SHA: ${gitSha}"
-  //
-  //   if (config.slackChannelName){
-  //     slackSend channel:"#${slackChannelName}",
-  //               color:'good',
-  //               message:"*START* Build of ${gitSha}:${env.JOB_NAME} - ${env.BUILD_NUMBER}\n(${env.BUILD_URL})\n *Build started by* :${getuser()}"
-  //   }
-  // }
-
   stage("unit-tests"){
     dockerBuilder {
-        gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
-        gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
-        gitSha  = "develop"
+        def gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
+        def gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
+        def gitSha  = "develop"
+
         dockerImageName = "cachet-backend"
         dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
         slackChannelName = 'jenkins'
@@ -56,11 +39,36 @@ def call(body) {
     }
   }
 
+  stage("lint"){
+    dockerBuilder {
+        gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
+        gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
+        gitSha  = "develop"
+
+        dockerImageName = "cachet-backend"
+        dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
+        slackChannelName = 'jenkins'
+
+        dockerEnvTag = "lint"
+        dockerDockerfile = "Dockerfile.lint"
+        dockerNoTagCheck = "true"
+        dockerSourceRelativePath = "backend"
+    }
+
+    dockerRunner {
+      dockerImageName = "cachet-backend"
+      dockerImageTag = "lint"
+      dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
+      slackChannelName = 'jenkins'
+    }
+  }
+
   stage("build-image") {
     dockerBuilder {
         gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
         gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
         gitSha  = "develop"
+
         dockerImageName = "cachet-backend"
         dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
         slackChannelName = 'jenkins'
