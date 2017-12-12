481ea851189a5c7482a5011f3853d5b0bb09c5e4
Saul Ortigoza <saul.ortigoza@wizeline.com>
2017-12-12 11:51:18 -0600

    generic dispatcher

---
+++ vars/genericDispatcher.groovy
@@ -0,0 +1,51 @@
+//#!Groovy
+def call(body) {
+
+  def config = [:]
+
+  if (body) {
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+  }
+
+  print config
+
+  stage("unit-tests"){
+    dockerBuilder {
+        gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
+        gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
+        gitSha  = "develop"
+        dockerImageName = "cachet-backend"
+        dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
+        slackChannelName = 'jenkins'
+
+        dockerEnvTag = "test"
+        dockerDockerfile = "Dockerfile.unit-tests"
+        dockerNoTagCheck = "true"
+        dockerSourceRelativePath = "backend"
+    }
+
+    dockerRunner {
+      dockerImageName = "cachet-backend"
+      dockerImageTag = "test"
+      dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
+      slackChannelName = 'jenkins'
+    }
+  }
+
+  stage("build-image") {
+    dockerBuilder {
+        gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
+        gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
+        gitSha  = "develop"
+        dockerImageName = "cachet-backend"
+        dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
+        slackChannelName = 'jenkins'
+
+        dockerSourceRelativePath = "backend"
+    }
+  }
+
+
+}
