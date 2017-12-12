2db87234c931ffe6030bf72d54dd7a1a4b22782c
Saul Ortigoza <saul.ortigoza@wizeline.com>
2017-12-12 11:22:43 -0600

    get user try catch sandboxed error

--- vars/getuser.groovy
+++ vars/getuser.groovy
@@ -1,9 +1,14 @@
 def call(){
-  
-  def build = currentBuild.rawBuild
-  def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
-  def name = cause.getUserName()
-  return name
+
+  try {
+    def build = currentBuild.rawBuild
+    def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+    def name = cause.getUserName()
+    return name
+  catch(Exception e) {
+    println "Exception ${e.message}. This errors occur when script runs in the sandbox"
+    return "Unknown"
+  }
 
     //wrap([$class: 'BuildUser']) {
     //  return BUILD_USER
