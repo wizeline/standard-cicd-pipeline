b0b39e58c3d44e274478ccc732d8360028d9e793
Saul Ortigoza <saul.ortigoza@wizeline.com>
2017-12-12 12:33:42 -0600

    closure local vars

--- vars/genericDispatcher.groovy
+++ vars/genericDispatcher.groovy
@@ -11,15 +11,15 @@ def call(body) {
 
   print config
 
-  def gitRepoUrl = params.GIT_REPO_URL
-  def gitCredentialsId = params.GIT_CREDENTIALS_ID
-  def gitSha = params.BRANCH
+  def jobGitRepoUrl = params.GIT_REPO_URL
+  def jobGitCredentialsId = params.GIT_CREDENTIALS_ID
+  def jobGitSha = params.BRANCH
 
   stage("unit-tests"){
     dockerBuilder {
-        def gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
-        def gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
-        def gitSha  = "develop"
+        gitRepoUrl = "git@github.com:wizeline/wz-statuspage.git"
+        gitCredentialsId = 'b9abf261-0552-45f2-972d-08f3800d3d4f'
+        gitSha  = "develop"
 
         dockerImageName = "cachet-backend"
         dockerRegistryCredentialsId = 'd656f8b1-dcf6-4737-83c1-c9199fb02463'
