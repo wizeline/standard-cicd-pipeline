d7e9c14ad4ff3098f1d2689fdebcc99127b30e88
Saul Ortigoza <saul.ortigoza@wizeline.com>
2017-12-15 11:39:08 -0600

    jenkins protocol 4

--- k8s/jenkins/deployments/create_secret.sh
+++ k8s/jenkins/deployments/create_secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl -n jenkins delete secret jenkins 
-kubectl -n jenkins create secret generic jenkins --from-file=./options
+kubectl -n jenkins delete secret jenkins
+kubectl -n jenkins create secret generic jenkins --from-file=./options --from-file=secret.slave-1 --from-file=secret.slave-2
