83aa0775eff6c7c7675961893f58ef44ad4f896e
Saul Ortigoza <saul.ortigoza@wizeline.com>
2018-02-26 14:30:35 -0600

    jenkins k8s deploy manifests

--- k8s/jenkins/deployments/create_secret.sh
+++ k8s/jenkins/deployments/create_secret.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+# SECRET_SLAVE_1=$(python - <<EOF
+# import uuid
+# print(str(uuid.uuid4()))
+# EOF)
+#
+# SECRET_SLAVE_2=$(python - <<EOF
+# import uuid
+# print(str(uuid.uuid4()))
+# EOF)
+
+SECRET_SLAVE_1=f447cf73c6f5ef74b0d7745590d82893d5a86132b6977d184ae2a457183a9b02
+SECRET_SLAVE_2=77a16b50be44d2b633243321359e253c5a9ce8045cd21e72b901e31707111852
+JENKINS_OPTIONS="$(tr -d '\n' < options)"
+
 kubectl -n jenkins delete secret jenkins
-kubectl -n jenkins create secret generic jenkins --from-file=./options --from-file=secret.slave-1 --from-file=secret.slave-2
+kubectl -n jenkins create secret generic jenkins \
+        --from-literal=options="$JENKINS_OPTIONS" \
+        --from-literal=secret.slave-1=$SECRET_SLAVE_1 \
+        --from-literal=secret.slave-2=$SECRET_SLAVE_2
