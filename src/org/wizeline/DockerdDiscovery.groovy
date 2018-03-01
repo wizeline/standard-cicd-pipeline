ff4c3e042214d389b119f2ca86cb27a71f5ad901
Saul Ortigoza <saul.ortigoza@wizeline.com>
2018-03-01 11:41:47 -0600

    fixing docker discovery errors

--- src/org/wizeline/DockerdDiscovery.groovy
+++ src/org/wizeline/DockerdDiscovery.groovy
@@ -1,7 +1,7 @@
 package org.wizeline
 
 public class DockerdDiscovery implements Serializable {
-  @NonCPS
+  // @NonCPS
   static def getDockerDaemon(steps, dockerDaemonHost, dockerDaemonPort, dockerDaemonDnsDiscovery) {
       if (!dockerDaemonHost){
         dockerDaemonHost = steps.sh(script: "dig +short ${dockerDaemonDnsDiscovery} | head -n 1", returnStdout: true).trim()
