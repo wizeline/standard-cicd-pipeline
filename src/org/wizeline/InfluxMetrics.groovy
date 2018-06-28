17ed6b381f212b82e6ae34a8a577822cff85b532
Saul Ortigoza <saul.ortigoza@wizeline.com>
2018-06-28 15:39:37 -0500

    test

--- src/org/wizeline/InfluxMetrics.groovy
+++ src/org/wizeline/InfluxMetrics.groovy
@@ -83,8 +83,6 @@ public class InfluxMetrics implements Serializable {
     def data = "${this.measurement_name},${measurement_tags} ${values}"
     def post_url = "${this.influxURL}/write?db=${this.influxDb}"
 
-    this.steps.echo(data)
-
     this.sendPostRequest(post_url, data)
   }
 
