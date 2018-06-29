217da4c2e9f7b24f94ef8ec1d1731e383a1baa12
Saul Ortigoza <saul.ortigoza@wizeline.com>
2018-06-29 16:48:59 -0500

    test

--- src/org/wizeline/BuildManifest.groovy
+++ src/org/wizeline/BuildManifest.groovy
@@ -8,7 +8,7 @@ public class BuildManifest implements Serializable {
   private config
   private build_user
   private git_info
-  private job_type
+  private String job_type
 
   BuildManifest(steps, params, env, config, build_user, git_info, job_type) {
     this.steps = steps
@@ -20,12 +20,12 @@ public class BuildManifest implements Serializable {
     this.job_type = job_type
   }
 
-  // @NonCPS
+  @NonCPS
   private def build_manifest(){
     // def curr_date = new Date()
 
-    def values =  {
-      "jenkins": {
+    def values =  [
+      "jenkins": [
         // "build_datetime": curr_date.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
         // "build_timestamp": this.steps.currentBuild.startTimeInMillis,
         "build_number": this.env.BUILD_NUMBER,
@@ -34,20 +34,20 @@ public class BuildManifest implements Serializable {
         "build_tag": this.env.BUILD_TAG,
         "build_user": this.build_user,
         "job_type": this.job_type,
-      },
-      "git": {
+      ],
+      "git": [
         "git_branch": this.git_info["git-branch"],
         "git_sha": this.git_info["git-commit-sha"],
         "git_author": this.git_info["git-author"],
         "git_sha_arg": this.git_info["git-sha-arg"],
         "git_repo_url": this.git_info["git-repo-url"],
-      }
-    }
+      ]
+    ]
 
     return values
   }
 
-  // @NonCPS
+  @NonCPS
   public def generate(){
     // def manifest = this.build_manifest()
     // println()
